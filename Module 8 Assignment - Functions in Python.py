31153ad7845e81871f61966b4287f31c45f35075
jonathanclee1 <jonathanclee1@gmail.com>
2025-07-31 11:15:03 -0400

    Add files via upload

---
+++ Module 8 Assignment - Functions in Python.py
@@ -0,0 +1,14 @@
+def greater_than(x, y):
+    return x > y
+
+# First test
+a = 2
+b = 3
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+# Second test
+a = 10
+b = 6
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
